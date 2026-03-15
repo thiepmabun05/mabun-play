52fa60553c89c370c44b86a5ddb1de43bf5b03fc
thiepmabun05 <thiepmabun05@gmail.com>
2026-03-15 19:45:03 +0200

    Update config.js

--- Mabun2.0/js/core/config.js
+++ Mabun2.0/js/core/config.js
@@ -1,6 +1,6 @@
 // Environment configuration – values can be overridden by window.__APP_CONFIG__
 const defaults = {
-  API_BASE_URL: 'https://ozydwqmnfdheqfcycatd.supabase.co',
+  API_BASE_URL: 'https://ozydwqmnfdheqfcycatd.functions.supabase.co',
   SUPABASE_URL: 'https://ozydwqmnfdheqfcycatd.supabase.co',
   SPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96eWR3cW1uZmRoZXFmY3ljYXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NjYyNDMsImV4cCI6MjA4OTE0MjI0M30.inot98a-xy8R5pJvdGETqIz-QGeP-3JQgEn1b4ljlcI',
   MIN_DEPOSIT: 5000,
