c3bb512dfc1cbddddea335746d1fc878020d6daa
thiepmabun05 <thiepmabun05@gmail.com>
2026-03-15 19:58:13 +0200

    Update config.js

--- Mabun2.0/js/core/config.js
+++ Mabun2.0/js/core/config.js
@@ -2,7 +2,7 @@
 const defaults = {
   API_BASE_URL: 'https://ozydwqmnfdheqfcycatd.functions.supabase.co',
   SUPABASE_URL: 'https://ozydwqmnfdheqfcycatd.supabase.co',
-  SPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96eWR3cW1uZmRoZXFmY3ljYXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NjYyNDMsImV4cCI6MjA4OTE0MjI0M30.inot98a-xy8R5pJvdGETqIz-QGeP-3JQgEn1b4ljlcI',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96eWR3cW1uZmRoZXFmY3ljYXRkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM1NjYyNDMsImV4cCI6MjA4OTE0MjI0M30.inot98a-xy8R5pJvdGETqIz-QGeP-3JQgEn1b4ljlcI',
   MIN_DEPOSIT: 5000,
   MAX_WALLET: 50000000,
   WITHDRAWAL_FEE: 0.01, // 1%
