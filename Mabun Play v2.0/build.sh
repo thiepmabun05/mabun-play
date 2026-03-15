8647346df2e36635becff2c89f53b59b860ca7f7
thiepmabun05 <thiepmabun05@gmail.com>
2026-03-15 17:06:59 +0200

    Delete Mabun Play v2.0 directory

--- Mabun Play v2.0/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# build.sh – replaces placeholders with environment variables
-
-# Copy config.js to a temporary location or process in place
-sed -i "s|__API_BASE_URL__|${API_BASE_URL}|g" js/core/config.js
-sed -i "s|__SUPABASE_URL__|${SUPABASE_URL}|g" js/core/config.js
-sed -i "s|__SUPABASE_ANON_KEY__|${SUPABASE_ANON_KEY}|g" js/core/config.js
-
-echo "Config updated."
